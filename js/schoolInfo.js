c4a194921e01528c4ab4e83ff4b4538fbdf163e9
Wudji <1215168278@qq.com>
2024-06-29 16:51:01 +0800

    update2

---
+++ js/schoolInfo.js
@@ -0,0 +1,26 @@
+var schools = [
+    {
+        id: "NKU",
+        displayName: "南开大学（八里台校区）",
+        lng: "117.167328",
+        lat: "39.103715",
+        students: [
+            {name:"学生甲", major:"法学"},
+            {name:"学生乙", major:"工科试验班（信息计算科学）"}
+        ]
+    },
+    {
+        id: "HUST",
+        displayName: "华中科技大学",
+        lng: "114.413301",
+        lat: "30.513197",
+        students: [
+            {name:"学生丙", major:"未来技术学院"},
+            {name:"学生丁", major:"芝士专业名称"},
+            {name:"学生1", major:"名称1"},
+            {name:"学生2", major:"名称2"},
+            {name:"学生3", major:"EE"},
+            {name:"学生4", major:"工科试验班（智能医学工程）"}
+        ]
+    }
+]
